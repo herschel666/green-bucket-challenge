69943fc591aa03bbf0bbcaeda84db2c97ba51008
Emanuel Kluge <mail@emanuel-kluge.de>
2019-10-25 23:20:56 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import enTranslations from '@shopify/polaris/locales/en.json';
+import { AppProvider } from '@shopify/polaris';
+
+import { Login } from './pages/login/login';
+
+function App() {
+  return (
+    <AppProvider i18n={enTranslations}>
+      <Router>
+        <Switch>
+          <Route path="/login" exact={true} component={Login} />
+          <Redirect from="/" to="/login" />
+        </Switch>
+      </Router>
+    </AppProvider>
+  );
+}
+
+export default App;
---
+++ src/pages/login/login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Page, Card } from '@shopify/polaris';
+
+export const Login = () => (
+  <Page title="Green Bucket Challenge">
+    <Card title="😜">
+      <h1>Hello World!</h1>
+    </Card>
+  </Page>
+);
